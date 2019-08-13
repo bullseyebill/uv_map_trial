5691b489bbdf58496b4a5a131d4b53c34d31b1b8
Ubuntu <ubuntu@ip-172-31-36-196.ec2.internal>
2019-08-13 17:57:18 +0000

    data handling

--- assets/js/map-information.js
+++ assets/js/map-information.js
@@ -1,37 +1,30 @@
 function fetchGoogleInformation(event) {
-    
-    var place =$("address").val();
+
+    var place = $("#address").val();
     if (!place) {
         $("#gm-user-data").html(`<h2>Please enter location</h2>`);
-    return;
+        return;
+    }
 }
 
 
-/*
-Open UV Javascript request
+function getUVIndex(lat, lng) {
 
-function getUVIndex() {
- var lat = $('#lat').val();
- var lng = $('#lng').val();
- var alt = $('#alt').val();
- var ozone = $('#ozone').val();
- var dt = $('#dt').val();
-
- $.ajax({
-   type: 'GET',
-   dataType: 'json',
-   beforeSend: function(request) {
-     request.setRequestHeader('x-access-token', 'YOUR_API_KEY');
-   },
-   url: 'https://api.openuv.io/api/v1/uv?lat=' + lat + '&lng=' + lng + '&alt=' + alt + '&ozone=' + ozone + '&dt=' + dt,
-   success: function(response) {
-     //handle successful response
-   },
-   error: function(response) {
-     // handle error response
-   }
- });
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        beforeSend: function(request) {
+            request.setRequestHeader('x-access-token', 'a32799ed6a34e3b2db9df5e1f53fb64c');
+        },
+        url: 'https://api.openuv.io/api/v1/uv?lat=' + lat + '&lng=' + lng,
+        success: function(response) {
+          console.log(response)
+        },
+        error: function(response) {
+            // handle error response
+        }
+    });
 }
-*/
+
 
 
